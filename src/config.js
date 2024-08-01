f2d7493842b2f416b58d9f82611c76801998775f
harshsonwane <harshsonwane05@gmail.com>
2024-08-01 10:28:20 +0530

    some changes  main repo

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-// replace below firebase config with your own
-
+//firebase key
 const firebaseConfig = {
       apiKey: "AIzaSyCORdS_5YLA9Gv8lPokHAyHKnSdFWfK6Mg",
       authDomain: "netflixcloneapp-42ff1.firebaseapp.com",
@@ -10,7 +9,7 @@ const firebaseConfig = {
       measurementId: "G-F7PKJQW93E"
   };
 
-// craete a/c on themoviedb.org and replace access token below
+// themoviedb.org key
 
 
 const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMmRkYTJkNDk5MjA5MDU5MDAxZjgyZmQ1NzZmMmQ5NyIsIm5iZiI6MTcyMjIzODg1NC4xNzQyNDIsInN1YiI6IjY2YTc0NTU4YWY5ZDMyZDMyYWFiYWFiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.G6UA_wDIsdVlSNt9hUyv0Qk6SPH4LGrX65oDsuPNAxQ";
